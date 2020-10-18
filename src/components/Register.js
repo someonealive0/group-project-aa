6a51bf44035b99ebbefba851670cc2503523e5ee
Nathan Soares <nsoaresbusiness@gmail.com>
2020-10-19 03:35:41 +1100

    Refactored register form and linked to firebase

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,90 +1,77 @@
 import React from 'react';
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import * as firebase from 'firebase'
+import { Redirect, Link } from 'react-router-dom';
+
+//Material-UI
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import Link  from 'react-router-dom/Link';
-import { Redirect } from 'react-router-dom';
 
 const Register = () => {
   const [open, setOpen] = useState(false)
+
+  const initialState = { username: '', password: '', email: '' }
+  const [signupDetails, setSignupDetails] = useState(initialState)
+  const [passwordRepeat, setPasswordRepeat] = useState('')
+  const regexUsername = /(\w+)/g
+
   const handleClickOpen = () => {
     setOpen(true);
   };
 
   const handleClose = () => {
     setOpen(false);
-    setsignupDetails(initialState)
+    setSignupDetails(initialState)
     setPasswordRepeat('')
   };
 
-  const handleSubmit = () => {
-    console.log({signupDetails});
-    console.log(passwordRepeat);
-    handleClose()
-  }
-  const initialState = {username:'', password:'', email:''}
-  const [signupDetails, setsignupDetails] = useState(initialState)
-    const [passwordRepeat, setPasswordRepeat] = useState('')
-    const regexUsername = /(\w+)/g
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const { username, email, password } = { ...signupDetails }
 
-    const handleSignupDetailsSubmit = (event) => {
-      event.preventDefault()
-
-      //Make sure fields aren't empty
-      if (!signupDetails.username || !signupDetails.password) {
-        console.log("Username or password is missing")
-        alert("Username or password is missing")
-        return
-      }
-      //Disallow spaces in username
-      if (signupDetails.username.split(' ').length != 1) {
-        console.log("Username can't contain spaces")
-        alert("Username can't contain spaces")
-        return
-      }
-      //Disallow non-alphanumeric characters
-      if (signupDetails.username.match(regexUsername)[0] != signupDetails.username) {
-        console.log("Username can only contain alphanumeric characters")
-        alert("Username can only contain alphanumeric characters")
-        return
-      }
-      //Confirm password
-      if (signupDetails.password != passwordRepeat) {
-        console.log("Passwords don't match")
-        alert("Passwords don't match")
-        return    
-      }
-      
-      firebase.auth().createUserWithEmailAndPassword(signupDetails.email, signupDetails.password).then(() => {
-        setsignupDetails({username:'', password:'', email:''})
-        setPasswordRepeat('')  
-      }).catch((error) => {
-        // Handle Errors here.
-        console.log(error.code)
-        console.log(error.message)
-        // ...
-      })
+    //Check for empty fields
+    if (!(username && email && password && passwordRepeat)) {
+      alert("Missing field")
+      console.log("Missing field")
+      return
     }
-    
-    const handleSignupDetailsChange = (prop) => (
-      (event) => {
-        setsignupDetails({...signupDetails, [prop]:event.target.value})
-      }
-    )  
-  const [username, setusername] = useState('')
-  const [password, setpassword] = useState('')
+
+    //Validate username
+    if (username.split(' ').length != 1 || username.match(regexUsername)[0] != username) {
+      console.log("Invalid username")
+      alert("Username can't contain spaces")
+      return
+    }
+
+    //Validate password
+    if (password.length < 6) {
+      console.log("Password must contain at least 6 characters")
+      alert("Password must contain at least 6 characters")
+      return
+    }
+    if (password != passwordRepeat) {
+      console.log("Passwords don't match")
+      alert("Passwords don't match")
+      return
+    }
+
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(() => {
+      setSignupDetails({ 'username': '', 'password': '', 'email': '' })
+      setPasswordRepeat('')
+    }).catch((error) => console.log(error.message))
+  }
+
   return (
     <div>
-      <Link onClick={handleClickOpen}>
-        Register
-      </Link>
+      <Link onClick={handleClickOpen}>Register</Link>
+
       <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
         <DialogTitle id="form-dialog-title">Register</DialogTitle>
+
         <DialogContent>
           <TextField
             autoFocus
@@ -92,29 +79,42 @@ const Register = () => {
             id="name"
             label="Username"
             type="text"
-            value = {username}
+            value={signupDetails.username}
             fullWidth
-            onChange={(e) => setusername(e.target.value)}
+            onChange={(e) => setSignupDetails({ ...signupDetails, 'username': e.target.value })}
           />
 
+          <TextField
+            margin="dense"
+            id="name"
+            label="Email"
+            type="text"
+            value={signupDetails.email}
+            fullWidth
+            onChange={(e) => setSignupDetails({ ...signupDetails, 'email': e.target.value })}
+          />
 
           <TextField
             margin="dense"
             id="password"
             label="Password"
             type="password"
+            value={signupDetails.password}
             fullWidth
-            onChange={(e) => setpassword(e.target.value)}
+            onChange={(e) => setSignupDetails({ ...signupDetails, 'password': e.target.value })}
           />
+
           <TextField
             margin="dense"
             id="password"
             label="Confirm Password"
             type="password"
+            value={passwordRepeat}
             fullWidth
-            onChange={(e) => setpassword(e.target.value)}
+            onChange={(e) => setPasswordRepeat(e.target.value)}
           />
         </DialogContent>
+
         <DialogActions>
           <Button onClick={handleClose} color="primary">
             Cancel
@@ -123,6 +123,7 @@ const Register = () => {
             Register
           </Button>
         </DialogActions>
+
       </Dialog>
     </div>
   );
