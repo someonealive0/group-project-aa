d68f74e5c09104e7d0cd6b7f150ba835ca7c320a
Bhavya Bhavsar <39274710+bhavya-bhavsar@users.noreply.github.com>
2020-10-20 16:37:51 +1100

    Animation on landing page

---
+++ src/components/Spinner.js
@@ -0,0 +1,12 @@
+import smile from "../css/smile.png"; //Icon by Vitaly Gorbachev https://www.flaticon.com/authors/vitaly-gorbachev"
+import "../css/App.css";
+import React from "react";
+
+const Spinner = () => {
+  return (
+    
+      <img src={smile} alt="" className="spinner"></img>
+  );
+};
+
+export default Spinner;
---
+++ src/components/styles/styles.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {makeStyles} from '@material-ui/core/styles'
+const styles = makeStyles({
+    submitBtn : {
+        margin : 'auto'
+    }
+    
+})
+
+export default styles
