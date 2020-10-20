54d2f2a967cfcfd5c5bf64ee822917bc370c56cf
Nathan Soares <nsoaresbusiness@gmail.com>
2020-10-20 22:17:12 +1100

    Added user dashboard styling

--- src/components/DashboardView.js
+++ src/components/DashboardView.js
@@ -7,7 +7,7 @@ const MessageView = () => {
     const [user, setUser] = useState(undefined)
     const testChannelNames = ["general", "meme", "uni chat"]
     const testGroupName = "COMP3160 Group Project" //Should be <= 22 chars
-    const testChannelDescription = "This is a description for the COMP3160 channel"
+    const testChannelDescription = "This is a description for the general channel"
 
     useEffect(() => {
         const unsubscribe = firebase.auth().onAuthStateChanged((authState) => {
@@ -23,6 +23,7 @@ const MessageView = () => {
     }, [])
 
     if (user === null) return (<Redirect to="/" />) //Redirect to landing page if user logged out
+    if (user === undefined) return (<></>) //User hasn't initialised yet
     return (
         <div className="dbWrapper">
             <div className="dbHeader">Discord (sort of)</div>
@@ -43,7 +44,10 @@ const MessageView = () => {
                             </li>
                         ))}
                     </ul>
-                    <div className="dbUserInfo"></div>
+                    <div className="dbUserInfo">
+                        <div className="dbUserImg"><img src={"/smile.png"}></img></div>
+                        <span className="dbUsername">{user.uid}</span>
+                    </div>
                 </div>
                 <div className="dbMessagesCol">
                     <div className="dbColHeader">
